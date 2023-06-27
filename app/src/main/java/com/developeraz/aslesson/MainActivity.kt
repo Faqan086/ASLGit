68aaffd4a3fcb9ecf327cbc4bab2ee6be3b8769c
Faqan086 <faqanaliev@gmail.com>
2023-06-27 09:15:11 +0400

    Update 3 MainActivity Functions Lesson

--- app/src/main/java/com/developeraz/aslesson/MainActivity.kt
+++ app/src/main/java/com/developeraz/aslesson/MainActivity.kt
@@ -2,6 +2,11 @@ package com.developeraz.aslesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var counter = 0
@@ -9,26 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val txtASL = findViewById<TextView>(R.id.textView)
+        val btnASL = findViewById<Button>(R.id.button)
+        txtASL.text = "Application"
+        btnASL.text = "Dont push"
+    }
+    fun onClickButton(view: View) {
+        val txtASL = findViewById<TextView>(R.id.textView)
+        txtASL.text = "You did not listen me!"
+        txtASL.textSize = 30F
+        val btnASL = findViewById<Button>(R.id.button)
+        counter++
+        btnASL.text = counter.toString()
     }
 
-    override fun onStart() {
-        super.onStart()
-    }
 
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
 }
